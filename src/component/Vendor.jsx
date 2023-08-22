a98690d299759bda6a9aba661c2cac58779d1aec
nGhart <107068059+nGhart@users.noreply.github.com>
2023-08-22 15:43:06 +0000

    Update Vendor.jsx edit

--- src/component/Vendor.jsx
+++ src/component/Vendor.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import "./Vendor.css";
-import { Container } from "react-bootstrap";
+import React from 'react';
+import './Vendor.css';
+import { Container } from 'react-bootstrap';
 
 const Vendor = (props) => {
   const { img, tradename, number, phonenumber } = props;
   console.log(number);
   return (
     <Container fluid className="vendor">
-      {" "}
-      <ariticle className="">
+      {' '}
+      <article className="">
         {/* <span className="number">#{number + 1}</span> */}
         <img src={img} alt={tradename} />
         <h2>{tradename}</h2>
         <p>{phonenumber}</p>
         {/* <h4>{fname}</h4> */}
-      </ariticle>
+      </article>
     </Container>
   );
 };
